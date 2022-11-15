661c5c8370ef3f194630643c311c676350497a8f
keshav <69523389+keshavkadale@users.noreply.github.com>
2022-11-15 14:55:02 +0530

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,11 +1,12 @@
 
+from io import BytesIO
+
+import numpy as np
+import tensorflow as tf
+import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-import numpy as np
-from io import BytesIO
 from PIL import Image
-import tensorflow as tf
 
 app = FastAPI()
 
@@ -23,7 +24,44 @@ app.add_middleware(
 
 MODEL = tf.keras.models.load_model("D:/potato-disease-classification-main/potato-disease-classification-main/saved_models/model_resnet152V2.h5")
 
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+CLASS_NAMES = ["Apple___Apple_scab",
+ "Apple___Black_rot",
+ "Apple___Cedar_apple_rust",
+ "Apple___healthy",
+ "Blueberry___healthy",
+ "Cherry_(including_sour)___Powdery_mildew",
+ "Cherry_(including_sour)___healthy",
+ "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot",
+ "Corn_(maize)___Common_rust",
+ "Corn_(maize)___Northern_Leaf_Blight",
+ "Corn_(maize)___healthy",
+ "Grape___Black_rot",
+ "Grape___Esca_(Black_Measles)",
+ "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)",
+ "Grape___healthy",
+ "Orange___Haunglongbing_(Citrus_greening)",
+ "Peach___Bacterial_spot",
+ "Peach___healthy",
+ "Pepper,_bell___Bacterial_spot",
+ "Pepper,_bell___healthy",
+ "Potato___Early_blight",
+ "Potato___Late_blight",
+ "Potato___healthy",
+ "Raspberry___healthy",
+ "Soybean___healthy",
+ "Squash___Powdery_mildew",
+ "Strawberry___Leaf_scorch",
+ "Strawberry___healthy",
+ "Tomato___Bacterial_spot",
+ "Tomato___Early_blight",
+ "Tomato___Late_blight",
+ "Tomato___Leaf_Mold",
+ "Tomato___Septoria_leaf_spot",
+ "Tomato___Spider_mites Two-spotted_spider_mite",
+ "Tomato___Target_Spot",
+ "Tomato___Tomato_Yellow_Leaf_Curl_Virus",
+ "Tomato___Tomato_mosaic_virus",
+ "Tomato___healthy"]
 
 @app.get("/ping")
 async def ping():
